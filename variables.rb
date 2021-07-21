18753fbfeca855b9efb36961d1101572330a2360
Andrew Smith <c13andrew.smith@gmail.com>
2021-07-21 10:17:20 -0700

    Add Part II

---
+++ variables.rb
@@ -0,0 +1,9 @@
+famous_person = "Albert Einstein"
+is_active = false
+famous_person = "Cher"
+num_3 = 3
+num_9 = 9
+sum = num_3 + num_9
+animals = ["lions", "tigers", "bears"]
+animals.shift
+puts famous_person.length
