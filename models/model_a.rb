c87e528013a8a06c84ef503590c9552a120d0a6a
Andrew Smith <c13andrew.smith@gmail.com>
2021-07-21 10:29:58 -0700

    Add models/model_a.rb school hash

---
+++ models/model_a.rb
@@ -0,0 +1,10 @@
+school = {
+  name: "Waverly High School",
+  address: "123 Sunny Lane",
+  year_founded: 1965,
+  number_of_students: 635,
+  is_open: false,
+  is_fun: true,
+  subjects_taught: ["Math", "Science", "History"],
+  teachers: ["Roger Rogers", "Emma Alexander", "Stacey Barajas"]
+}
