cfbdeff38ab9fb6825a552a1e45efcd584ece71f
Andrew Smith <c13andrew.smith@gmail.com>
2021-07-21 10:35:35 -0700

    Add models/model_b.rb pet hash

---
+++ models/model_b.rb
@@ -0,0 +1,10 @@
+pet = {
+  name: "Quest",
+  type: "Dog",
+  age: 9,
+  weight_in_lbs: 55,
+  is_fluffy: true,
+  is_cute: true,
+  parents: ["Fire", "Venture"],
+  toys: ["soccer ball", "rubber bone", "teddybear"]
+}
